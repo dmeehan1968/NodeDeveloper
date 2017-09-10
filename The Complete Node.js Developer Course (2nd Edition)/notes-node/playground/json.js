4b6853ab24c2f7b8cbfd6010223348ed0940be62
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-10 13:56:37 +0100

    Write and Read json file

--- The Complete Node.js Developer Course (2nd Edition)/notes-node/playground/json.js
+++ The Complete Node.js Developer Course (2nd Edition)/notes-node/playground/json.js
@@ -1,5 +1,17 @@
-var personString = '{"name":"Dave", "age":49}';
-var person = JSON.parse(personString);
 
-console.log(typeof person);
-console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note)
+console.log('Title:', note.title);
