aece026d1133b3bd94b726cc834db414aec001fb
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-25 20:03:11 +0100

    Experiment with Dark Sky weather api

--- NodeDeveloper/weather-app/app.js
+++ NodeDeveloper/weather-app/app.js
@@ -1,24 +1,40 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode.js');
+// https://api.darksky.net/forecast/fff633074dbbabd4169bf2f892f54346/51.0371175,-2.826742
+// fff633074dbbabd4169bf2f892f54346
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
+const request = require('request');
+
+request({
+  url: "https://api.darksky.net/forecast/fff633074dbbabd4169bf2f892f54346/51.0371175,-2.826742",
+  json: true
+}, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(body.currently.temperature);
     }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
 });
