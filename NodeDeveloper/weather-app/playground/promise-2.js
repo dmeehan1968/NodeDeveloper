bab82c2b9d7e1cc88223b2c47e47482c42de218e
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-26 20:05:46 +0100

    Added geocode request and resolve promise

--- NodeDeveloper/weather-app/playground/promise-2.js
+++ NodeDeveloper/weather-app/playground/promise-2.js
@@ -1,10 +1,29 @@
+const request = require('request');
+
 var geocodeAddress = (address) => {
   return new Promise((resolve, reject) => {
-    resolve({
-      address: 'Dummy',
-      latitude: 0,
-      longitude: 0
-    })
+
+    var encodedAddress = encodeURIComponent(address);
+
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        reject('Unable to connect to Google servers');
+      } else if (body.status === 'ZERO_RESULTS') {
+        reject('Address not found');
+      } else if (body.status == "OK") {
+          resolve({
+            address: body.results[0].formatted_address,
+            lat: body.results[0].geometry.location.lat,
+            lng: body.results[0].geometry.location.lng
+          });
+      } else {
+        reject(undefined, body);
+      }
+    });
+
   });
 };
 
