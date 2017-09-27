5695413e31fc2113dcc54a4b8c6a2951d28f72c6
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-27 18:32:40 +0100

    Add square function and test

--- NodeDeveloper/node-tests/utils/utils.test.js
+++ NodeDeveloper/node-tests/utils/utils.test.js
@@ -3,6 +3,14 @@ const utils = require('./utils');
 it('should add two numbers', () => {
   var result = utils.add(33,11);
   if (result !== 44) {
-    throw new Error(`expected 44, got ${result}`);    
+    throw new Error(`expected 44, got ${result}`);
   }
 });
+
+
+it('should square a number', () => {
+  var result = utils.square(8);
+  if (result != 64) {
+    throw new Error(`expected 64, got ${result}`);    
+  }
+})
