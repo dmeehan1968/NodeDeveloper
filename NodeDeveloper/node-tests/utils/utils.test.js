07d2e6336538ba4c357cf3538e10df14f43d92cb
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-27 18:54:00 +0100

    Added setName method and test

--- NodeDeveloper/node-tests/utils/utils.test.js
+++ NodeDeveloper/node-tests/utils/utils.test.js
@@ -3,11 +3,34 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
   var result = utils.add(33,11);
-  expect(result).toBe(44);
+  expect(result).toBe(44).toBeA('number');
 });
 
 
 it('should square a number', () => {
   var result = utils.square(8);
-  expect(result).toBe(64);
+  expect(result).toBe(64).toBeA('number');
+});
+
+// it('should expect some values', () => {
+//
+//   // expect({ name: 'Andrew' }).toEqual({ name: 'Andrew' });
+//   // expect({ name: 'Andrew', age: 44, location: 'London' }).toInclude({age: 44});
+//
+// });
+
+it('should verify first and last names are set', () => {
+
+  var user = {
+    age: 25,
+    location: 'London'
+  };
+
+  user = utils.setName(user, 'Dave Meehan');
+
+  expect(user)
+    .toBeA('object')
+    .toInclude({ firstName: 'Dave' })
+    .toInclude({ lastName: 'Meehan' });
+
 });
