bc08fa0d00b478673cc013bb98de7fdc7c4e3aa5
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-27 18:30:52 +0100

    Add error handling on test failure

--- NodeDeveloper/node-tests/utils/utils.test.js
+++ NodeDeveloper/node-tests/utils/utils.test.js
@@ -2,4 +2,7 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
   var result = utils.add(33,11);
+  if (result !== 44) {
+    throw new Error(`expected 44, got ${result}`);    
+  }
 });
