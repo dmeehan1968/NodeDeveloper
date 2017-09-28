84455316692141bdbd3ee9148aadbef0a9d64545
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-28 20:13:13 +0100

    Add describe blocks to server tests

--- NodeDeveloper/node-tests/server/server.test.js
+++ NodeDeveloper/node-tests/server/server.test.js
@@ -3,31 +3,44 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return error page', (done) => {
+describe('Server', () => {
 
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.',
-        name: 'Todo App v1.0'
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
 
-});
+    it('should return error page', (done) => {
 
-it('should return users', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.',
+            name: 'Todo App v1.0'
+          });
+        })
+        .end(done);
 
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body)
-        .toBeA('array')
-        .toInclude({ name: 'Dave', age: 49 });
-    })
-    .end(done);
+    });
+
+  });
+
+  describe('GET /users', () => {
+
+    it('should return users', (done) => {
+
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body)
+            .toBeA('array')
+            .toInclude({ name: 'Dave', age: 49 });
+        })
+        .end(done);
+
+    });
+
+  });
+
 
 });
