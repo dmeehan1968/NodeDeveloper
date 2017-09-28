e36d0b5efa4d9a3ab756e5717030551cea63f633
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-28 20:08:32 +0100

    Use describe block in Utils tests

--- NodeDeveloper/node-tests/utils/utils.test.js
+++ NodeDeveloper/node-tests/utils/utils.test.js
@@ -1,56 +1,52 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var result = utils.add(33,11);
-  expect(result).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-it('should async add two numbers', (done) => {
+  it('should add two numbers', () => {
+    var result = utils.add(33,11);
+    expect(result).toBe(44).toBeA('number');
+  });
 
-  utils.asyncAdd(3,4,(sum) => {
+  it('should async add two numbers', (done) => {
 
-    expect(sum).toBe(7).toBeA('number');
-    done();
+    utils.asyncAdd(3,4,(sum) => {
+
+      expect(sum).toBe(7).toBeA('number');
+      done();
+
+    });
+  });
+
+  it('should square a number', () => {
+    var result = utils.square(8);
+    expect(result).toBe(64).toBeA('number');
+  });
+
+  it('should async square a number', (done) => {
+
+      utils.asyncSquare(3, (square) => {
+
+          expect(square).toBe(9).toBeA('number');
+          done();
+
+      });
 
   });
-});
 
-it('should square a number', () => {
-  var result = utils.square(8);
-  expect(result).toBe(64).toBeA('number');
-});
+  it('should verify first and last names are set', () => {
 
-it('should async square a number', (done) => {
+    var user = {
+      age: 25,
+      location: 'London'
+    };
 
-    utils.asyncSquare(3, (square) => {
+    user = utils.setName(user, 'Dave Meehan');
 
-        expect(square).toBe(9).toBeA('number');
-        done();
-        
-    });
+    expect(user)
+      .toBeA('object')
+      .toInclude({ firstName: 'Dave' })
+      .toInclude({ lastName: 'Meehan' });
 
-});
-
-// it('should expect some values', () => {
-//
-//   // expect({ name: 'Andrew' }).toEqual({ name: 'Andrew' });
-//   // expect({ name: 'Andrew', age: 44, location: 'London' }).toInclude({age: 44});
-//
-// });
-
-it('should verify first and last names are set', () => {
-
-  var user = {
-    age: 25,
-    location: 'London'
-  };
-
-  user = utils.setName(user, 'Dave Meehan');
-
-  expect(user)
-    .toBeA('object')
-    .toInclude({ firstName: 'Dave' })
-    .toInclude({ lastName: 'Meehan' });
-
+  });
 });
