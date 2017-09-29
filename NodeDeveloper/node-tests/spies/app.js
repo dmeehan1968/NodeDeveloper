d0ceb2be2d79913e6409e8bb6137f0eb66360db6
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-29 18:39:58 +0100

    Create spies example

---
+++ NodeDeveloper/node-tests/spies/app.js
@@ -0,0 +1,12 @@
+let db = require('./db');
+
+module.exports.handleSignup = (email, password) => {
+
+    // check if the email already exists
+
+    // save the user to the database
+    db.saveUser({ email, password });
+
+    // send welcome email
+
+};
