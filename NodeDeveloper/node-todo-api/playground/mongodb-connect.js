ea8687be12a403dd9b53815f03c7c36c1daec47d
Dave Meehan <dave_meehan@replicated.co.uk>
2017-09-29 20:14:09 +0100

    Add user insertion

--- NodeDeveloper/node-todo-api/playground/mongodb-connect.js
+++ NodeDeveloper/node-todo-api/playground/mongodb-connect.js
@@ -7,15 +7,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB Server');
 
-  db.collection('Todos').insertOne({
-    text: 'Something to do',
-    completed: false
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert todo', err);
-    }
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //
+  // });
 
-    console.log(JSON.stringify(result.ops, undefined, 2));
+  db.collection('Users').insertOne({
+    name: 'Dave',
+    age: 49,
+    location: 'Ireland'
+  }, (err, result) => {
+
+      if (err) {
+        return console.log('Unable to insert todo', err);
+      }
+
+      console.log(JSON.stringify(result.ops, undefined, 2));
 
   });
 
