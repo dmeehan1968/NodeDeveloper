59e7b76b675fbdfb0f5cf6a877de68e5583c25a8
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-01 17:21:14 +0100

    Object destructuring experiment

--- NodeDeveloper/node-todo-api/playground/mongodb-connect.js
+++ NodeDeveloper/node-todo-api/playground/mongodb-connect.js
@@ -1,5 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const user = { name: 'Andrew', age: 25 };
+var {name} = user;
+
+console.log(name);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
@@ -19,20 +24,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Dave',
-    age: 49,
-    location: 'Ireland'
-  }, (err, result) => {
-
-      if (err) {
-        return console.log('Unable to insert todo', err);
-      }
-
-      console.log(result.ops[0]._id.getTimestamp());
-
-  });
-
+  // db.collection('Users').insertOne({
+  //   name: 'Dave',
+  //   age: 49,
+  //   location: 'Ireland'
+  // }, (err, result) => {
+  //
+  //     if (err) {
+  //       return console.log('Unable to insert todo', err);
+  //     }
+  //
+  //     console.log(result.ops[0]._id.getTimestamp());
+  //
+  // });
+  //
   db.close();
-
+  //
 });
