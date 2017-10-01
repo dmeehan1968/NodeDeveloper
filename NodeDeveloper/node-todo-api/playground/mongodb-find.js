d15f8afb1a44bc4f3361e4e7b2f893eece0b4081
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-01 17:30:53 +0100

    Find all

--- NodeDeveloper/node-todo-api/playground/mongodb-find.js
+++ NodeDeveloper/node-todo-api/playground/mongodb-find.js
@@ -7,6 +7,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB Server');
 
-  db.close();
+  db.collection('Todos').find({ completed: false }).toArray().then((docs) => {
+
+    console.log(JSON.stringify(docs, undefined, 2));
+
+  }, (err) => {
+
+    console.log('Unable to find documents');
+
+  });
+
+  // db.close();
 
 });
