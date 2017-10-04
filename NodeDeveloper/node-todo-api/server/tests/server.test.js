5260f7768ac1c470363a9391454273cda0fa3d53
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 17:07:22 +0100

    Test GET /todos

--- NodeDeveloper/node-todo-api/server/tests/server.test.js
+++ NodeDeveloper/node-todo-api/server/tests/server.test.js
@@ -77,4 +77,31 @@ describe('POST /todos', () => {
       });
   });
 
+  describe('GET /todos', () => {
+
+      it('should get all todos', (done) => {
+
+          request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+
+              expect(res.body.todos.length).toBe(testTodos.length);
+
+            })
+            .end((err, res) => {
+
+                if (err) {
+                  return done(err);
+                }
+
+                done();
+
+            });
+
+      });
+
+  });
+
+
 });
