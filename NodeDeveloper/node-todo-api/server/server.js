377d2b93ba5ca345760a9bc2c1044a6427e211f7
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 14:09:37 +0100

    Define Todo model and save an example

--- NodeDeveloper/node-todo-api/server/server.js
+++ NodeDeveloper/node-todo-api/server/server.js
@@ -6,15 +6,25 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 var Todo = mongoose.model('Todo', {
 
     text: {
-
+      type: String
     },
 
     completed: {
-
+      type: Boolean
     },
 
     completedAt: {
-
+      type: Number
     }
-    
+
+});
+
+var newTodo = new Todo({
+  text: 'Cook dinner'
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo',doc);
+}, (e) => {
+  console.log('Unable to save');
 });
