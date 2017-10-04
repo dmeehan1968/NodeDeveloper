124a7ab7ba2921e400942f6186d78cee0394e3e0
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 14:05:08 +0100

    Outline Todo object model

--- NodeDeveloper/node-todo-api/server/server.js
+++ NodeDeveloper/node-todo-api/server/server.js
@@ -2,3 +2,19 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+
+    text: {
+
+    },
+
+    completed: {
+
+    },
+
+    completedAt: {
+
+    }
+    
+});
