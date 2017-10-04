742b24636b39dae1db6f8bf0d9796f190607af68
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 20:29:13 +0100

    Play with findByIdAndRemove

--- NodeDeveloper/node-todo-api/playground/mongoose-remove.js
+++ NodeDeveloper/node-todo-api/playground/mongoose-remove.js
@@ -4,6 +4,12 @@ var { mongoose } = require('../server/db/mongoose');
 var { Todo } = require('../server/models/todo');
 var { User } = require('../server/models/user');
 
-Todo.remove({}).then((results) => {
-  console.log(results);
+// Todo.remove({}).then((results) => {
+//   console.log(results);
+// });
+
+// Todo.findOneAndRemove()
+
+Todo.findByIdAndRemove('59d536181281ad0318d9e0bf').then((todo) => {
+  console.log(todo);
 });
