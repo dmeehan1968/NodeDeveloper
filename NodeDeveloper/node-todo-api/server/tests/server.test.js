ca347c489c479d00c15d993a6e983162385cde6b
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 16:24:11 +0100

    Test for invalid data

--- NodeDeveloper/node-todo-api/server/tests/server.test.js
+++ NodeDeveloper/node-todo-api/server/tests/server.test.js
@@ -33,11 +33,35 @@ describe('POST /todos', () => {
 
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
-            done()
+            done();
         }).catch((e) => done(e));
 
       });
 
+  });
+
+  it('should not create todo with invalid body data', (done) => {
+
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.name).toBe('ValidationError')
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+
+          expect(todos.length).toBe(0);
+          done();
+
+        }).catch((e) => done(e));
+
+      });
   });
 
 });
