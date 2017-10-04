12334035e997d41259e29c77f933ddbd1c936d32
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 15:22:49 +0100

    Handle post of new todo

--- NodeDeveloper/node-todo-api/server/server.js
+++ NodeDeveloper/node-todo-api/server/server.js
@@ -8,6 +8,22 @@ var { User } = require('./models/User');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
 app.listen(port, () => {
 
   console.log(`Started on ${port}`);
