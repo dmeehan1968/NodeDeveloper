c929e161a0ffd3593d005e824538e196c667ab38
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-04 21:10:36 +0100

    Add PATCH /todos/:id

--- NodeDeveloper/node-todo-api/server/server.js
+++ NodeDeveloper/node-todo-api/server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -69,6 +70,37 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     res.status(200).send({ todo });
+
+  }).catch((e) => res.status(404).send());
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  var body = _.pick(req.body, [ 'text', 'completed' ]);
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  },
+  { new: true }).then((todo) => {
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
 
   }).catch((e) => res.status(404).send());
 
