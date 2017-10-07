13772edda826a15a355fa86b8838f880121bc5dd
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-07 16:14:05 +0100

    Implement and test DELETE /users/me/token

--- NodeDeveloper/node-todo-api/server/server.js
+++ NodeDeveloper/node-todo-api/server/server.js
@@ -130,6 +130,16 @@ app.get('/users/me', authenticate, (req, res) => {
 
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+  
+});
+
 app.post('/users/login', (req, res) => {
 
   var body = _.pick(req.body, [ 'email', 'password' ]);
@@ -139,7 +149,7 @@ app.post('/users/login', (req, res) => {
       return user.generateAuthToken().then((token) => {
 
         res.set('x-auth', token).send(user);
-        
+
       });
 
   }).catch((e) => {
