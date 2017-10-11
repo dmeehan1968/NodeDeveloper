3019152ad9cef3d10e8ec1c6a46fa35e8c2553a4
Dave Meehan <dave_meehan@replicated.co.uk>
2017-10-11 20:09:09 +0100

    Add jquery

--- NodeDeveloper/node-chat-app/server/server.js
+++ NodeDeveloper/node-chat-app/server/server.js
@@ -6,12 +6,14 @@ const socketIO = require('socket.io');
 const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
+const jqueryPath = path.join(__dirname, '../node_modules/jquery/dist');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+app.use('/libs/jquery', express.static(jqueryPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
